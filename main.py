18c42a98cac4743c349758ba3366587fd345b431
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-11-25 11:38:09 +0600

    Update main.py
    Easy and  medium difficulty level working

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import random
 import time
+p = "Nice Job!","WOW!","You're amazing!","You're awesome!","You're smart!","Damn!"
+n = "Oh no,","You are not smart,","You are not awesome,","You are not amazing,","That's sad"
+
+print("Welcome to the quiz game!")
 print("This is a game where I will choose a number between 1-9.")
+
 time.sleep(2)
 print("And you have to guess the number.")
 time.sleep(2)
@@ -19,21 +24,46 @@ time.sleep(2)
 print("Do you think you can win?")
 time.sleep(2)
 print("Let's find out!")
-number = 1,2,3,4,5,6,7,8,9
-c = random.choice(number)
+c = random.randint(0,9)
+positive = random.choice(p)
+negative = random.choice(n)
 guess_count = 1
 while guess_count <= 3:
   guess = int(input("Take a guess: "))
   guess_count += 1
   if guess == c:
-      print("Damn! you won!")
+      print(f"{positive} you won!")
       break
 else:
-   print("Oh no... you have lost like others!")
+   print(f"{negative}you have lost like others!")
    time.sleep(1)
    print(f"I choosed {c}")
    time.sleep(1)
-   print("Better lucknext time")
+   print("Better luck next time")
   
+
 ##Medium mode
 ##This part has been under work
+
+print(f"{name}, Lets step it up a bit")
+time.sleep(2)
+print("I am going to choose a number between 0-100")
+time.sleep(2)
+print("And you have to take a guess\nSame rule like last time you get to guess 3 times.")
+time.sleep(2)
+print("Ready?")
+time.sleep(1)
+print("Let's go!")
+number = random.randint(0, 100)
+guess_count = 0
+while guess_count < 3:
+    guess = int(input("Take a guess: "))
+    guess_count += 1
+    if guess == number:
+        print(f"{positive}, you won!")
+        break
+else:
+  print(f"{negative} you have lost!")
+  time.sleep(1)
+  print(f"I choosed {number}")
+  
